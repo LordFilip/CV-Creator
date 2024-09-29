ec277853079bd95603d117b88fd69d0b65a262a7
Filip <filipivanovic59@gmail.com>
2024-09-29 11:59:26 +0200

    Adding Personal Informations

--- src/components/CreatePageComponents/PersonalInforamtions.jsx
+++ src/components/CreatePageComponents/PersonalInforamtions.jsx
@@ -1,3 +1,25 @@
+import style from "./PersonalInformations.module.css";
+
 export default function PersonalInforamtions() {
-  return <div>Personal Information</div>;
+  return (
+    <div>
+      <div className={style.title}>
+        <h1>Personal Informations</h1>
+      </div>
+      <div className={style.content}>
+        <div className={`${style.left} ${style.inputs}`}>
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Phone" />
+        </div>
+        <div className={`${style.right} ${style.inputs}`}>
+          <input type="text" placeholder="Address" />
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="Country" />
+          <input type="text" placeholder="Postal Code" />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Createpage.jsx
+++ src/pages/Createpage.jsx
@@ -1,15 +1,8 @@
 import { Link, Outlet } from "react-router-dom";
-import { useState } from "react";
+
 import style from "./Createpage.module.css";
 
 export default function Createpage() {
-  const [selected, setSelected] = useState(1);
-
-  function handleLink(value) {
-    setSelected(value);
-    console.log(selected);
-  }
-
   return (
     <div className={style.container}>
       <div className={style.navbar}>
@@ -19,28 +12,6 @@ export default function Createpage() {
         </Link>
       </div>
       <div className={style.main}>
-        <div className={style.progress}>
-          <ul>
-            <li
-              className={selected === 1 ? style.selected : ""}
-              onClick={() => handleLink(1)}
-            >
-              <Link to="personalInformations">1</Link>
-            </li>
-            <li
-              className={selected === 2 ? style.selected : ""}
-              onClick={() => handleLink(2)}
-            >
-              <Link to="education">2</Link>
-            </li>
-            <li
-              className={selected === 3 ? style.selected : ""}
-              onClick={() => handleLink(3)}
-            >
-              <Link to="experience">3</Link>
-            </li>
-          </ul>
-        </div>
         <div className={style.content}>
           <Outlet />
         </div>
