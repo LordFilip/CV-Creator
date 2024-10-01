1683c1af2643feed64f5c593f80ad5ee11dd5ae0
Filip <filipivanovic59@gmail.com>
2024-10-01 21:24:11 +0200

    Icons

--- src/components/CreatePageComponents/Education.jsx
+++ src/components/CreatePageComponents/Education.jsx
@@ -10,30 +10,30 @@ export default function Education() {
         <div className={`${style.left} ${style.inputs}`}>
           <div className={style.inputWrapper}>
             <input type="text" placeholder="University name" />
-            <i className="fa-solid fa-user"></i>
+            <i className="fa-solid fa-school"></i>
           </div>
           <div className={style.inputWrapper}>
             <input type="text" placeholder="City" />
-            <i className="fa-solid fa-user"></i>
+            <i className="fa-solid fa-city"></i>
           </div>
           <div className={style.inputWrapper}>
-            <input type="email" placeholder="Degree" />
-            <i className="fa-solid fa-envelope"></i>
+            <input type="text" placeholder="Degree" />
+            <i className="fa-solid fa-graduation-cap"></i>
           </div>
           <div className={style.inputWrapper}>
             <input type="text" placeholder="Subject" />
-            <i className="fa-solid fa-home"></i>
+            <i className="fa-solid fa-book-open"></i>
           </div>
         </div>
 
         <div className={`${style.right} ${style.inputs}`}>
           <div className={style.inputWrapper}>
             <input type="text" placeholder="From (dd/mm/yy)" />
-            <i className="fa-solid fa-city"></i>
+            <i className="fa-solid fa-calendar-alt"></i>
           </div>
           <div className={style.inputWrapper}>
             <input type="text" placeholder="To (dd/mm/yy)" />
-            <i className="fa-solid fa-flag"></i>
+            <i className="fa-solid fa-calendar-check"></i>
           </div>
           <div className={`${style.inputWrapper} ${style.invisible}`}>
             <input type="text" placeholder="To (dd/mm/yy)" />
--- src/components/CreatePageComponents/Experience.jsx
+++ src/components/CreatePageComponents/Experience.jsx
@@ -1,6 +1,6 @@
 import style from "./Experience.module.css";
 
-export default function Education() {
+export default function Experience() {
   return (
     <div>
       <div className={style.title}>
@@ -10,38 +10,38 @@ export default function Education() {
         <div className={`${style.left} ${style.inputs}`}>
           <div className={style.inputWrapper}>
             <input type="text" placeholder="Position" />
-            <i className="fa-solid fa-user"></i>
+            <i className="fa-solid fa-briefcase"></i>
           </div>
           <div className={style.inputWrapper}>
             <input type="text" placeholder="Company" />
-            <i className="fa-solid fa-user"></i>
+            <i className="fa-solid fa-building"></i>
           </div>
           <div className={style.inputWrapper}>
-            <input type="email" placeholder="City" />
-            <i className="fa-solid fa-envelope"></i>
+            <input type="text" placeholder="City" />
+            <i className="fa-solid fa-city"></i>
           </div>
           <div className={`${style.inputWrapper} ${style.invisible}`}>
-            <input type="text" placeholder="To (dd/mm/yy)" />
-            <i className="fa-solid fa-flag"></i>
+            <input type="text" placeholder="Additional Info" />
+            <i className="fa-solid fa-info-circle"></i>
           </div>
         </div>
 
         <div className={`${style.right} ${style.inputs}`}>
           <div className={style.inputWrapper}>
             <input type="text" placeholder="From (dd/mm/yy)" />
-            <i className="fa-solid fa-city"></i>
+            <i className="fa-solid fa-calendar-alt"></i>
           </div>
           <div className={style.inputWrapper}>
             <input type="text" placeholder="To (dd/mm/yy)" />
-            <i className="fa-solid fa-flag"></i>
+            <i className="fa-solid fa-calendar-check"></i>
           </div>
           <div className={`${style.inputWrapper} ${style.invisible}`}>
-            <input type="text" placeholder="To (dd/mm/yy)" />
-            <i className="fa-solid fa-flag"></i>
+            <input type="text" placeholder="Additional Info" />
+            <i className="fa-solid fa-info-circle"></i>
           </div>
           <div className={`${style.inputWrapper} ${style.invisible}`}>
-            <input type="text" placeholder="To (dd/mm/yy)" />
-            <i className="fa-solid fa-flag"></i>
+            <input type="text" placeholder="Additional Info" />
+            <i className="fa-solid fa-info-circle"></i>
           </div>
         </div>
       </div>
